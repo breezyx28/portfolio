49daf54489f63704e84fecff4d427a20edea08c1
Mohamed Ahamed <mohamedx.28@gmail.com>
2021-03-31 11:08:40 +0100

    animation part

--- js/gsapAnime.js
+++ js/gsapAnime.js
@@ -2,6 +2,6 @@ import {gsap} from '../lib/gsap-public/gsap-public/src/gsap-core.js';
 //top nav
 gsap.from(".top-nav",{opacity: 0, y: -100, duration: 0.26});
 
-gsap.from(".front-wave",{opacity: 0, y: 100, duration: 0.36});
+gsap.from(".front-wave",{opacity: 0, y: 100, duration: 0.36,ease:'Power'});
 gsap.from(".back-wave",{opacity: 0, x: 30, duration: 0.66,delay:0.57});
 gsap.from(".pattern7x7",{opacity: 0, y: 10,x:50, duration: 0.46,delay:0.7});
